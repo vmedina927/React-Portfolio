7dd118f3cbc83ff3c1db8d78a2a6d10ec48af3c4
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-31 15:56:43 -0400

    Added new files and fixed errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import "./index.css";
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Page from './components/Page';
+import Footer from './components/Footer';
 
 function App() {
-  return <div></div>;
+  const [pages] = useState([
+    { name: 'about me'},
+    { name: 'portfolio'},
+    { name: 'contact'},
+    { name: 'resume'}
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  return(
+    <div>
+      <Header>
+        <Nav>
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        </Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
