d8eff83d17f1943da4b959f09e449e9a85a60d95
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-31 16:48:42 -0400

    Added code for index.js for the Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Footer() {
+
+    const icons = [
+        {
+            name: 'fab fa-github',
+            link: 'https://github.com/vmedina927'
+        },
+        {
+            name: 'fab fa-linkedin',
+            link: 'https://www.linkedin.com/in/vmedina927/'
+        },
+        {
+            name: 'fab fa-stack-overflow',
+            link: 'https://stackoverflow.com/users/18049556/vickiana'
+        }
+    ]
+    return (
+        <section>
+            <footer className="container">
+                <div className="row">
+                <div className="col-lg-8 mx-auto text-center">
+                    {icons.map(icon =>
+                    (
+                        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
+                    )
+                    )}
+                </div>
+            </div>
+        </footer>    
+    </section >
+    );
+}
+
+export default Footer;
