af87ca35a80524ef440728fdaf9da58f4f24be3a
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-08-03 00:21:45 -0400

    Files were changes and reinstall react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import './App.css';
-
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <>
     <div>
-      <Header />
-      <Footer />
+      <Header></Header>
     </div>
-  )
+    </>
+  );
 }
-
 
 export default App;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
+import portfolioImage from '../../assets/images/myImage.jpeg';
+import Bootstrap from 'bootstrap';
 
 function About() {
-    return(
-        <section className="container">
-            <h2 class="top-title">Vickiana Medina</h2>
-            <hr></hr>
-            <div>
-                <img class="mb-5" id="avatar" src="src/assets/img/Portrait.jpeg" alt="Vickiana Medina" />
-
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-                condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-                mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-                </p>
-                <p>
-                Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-                justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-                dictumst.
-                </p>
-                <p>
-                Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-                </p>
+    return (
+        <section className="title">
+            <div className="row justify-content-center" id="aboutMe">
+                <div className="col-md-12 col-lg-6">
+                    <center>
+                        <img src={portfolioImage}
+                            id="my-image"
+                            alt="picture of Vickiana Medina" />
+                    </center>
+                    <div className="d-flex flex-column justify-content-center">
+                        <center>
+                            <h1 id="my-name">Vickiana Medina</h1>
+                            <h2 id="my-career">Coding Bootcamp Graduate</h2>
+                        </center>
+                        <p>
+                            orem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+                            condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+                            mi, ac feugiat metus aliquam maximus.
+                        </p>
+                        <p>
+                            Proin aliquam justo nec diam vulputate vestibulum.
+                            Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
+                            justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
+                            dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
-        )
+    );
 }
 
 export default About;
