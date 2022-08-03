7122a155dfdec1ba051de6f6b1f46e88ac24b156
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-08-03 12:47:07 -0400

    Added code for index.js for Footer and Nav component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,19 +5,19 @@ function Footer() {
         <div className="page-footer" id ="footer">
             <center>
                 <a target="blank" href="https://github.com/vmedina927">
-                    <img src="https://img.icons8.com/sf-black-filled/344/github.png"
+                    <img src="https://img.icons8.com/sf-black-filled/48/github.png"
                     alt="github-logo"
                     class="icon"
                     />
                     </a>
                     <a target="blank" href="https://www.linkedin.com/in/vmedina927/">
-                    <img src="https://img.icons8.com/ios/344/linkedin-circled--v1.png"
+                    <img src="https://img.icons8.com/ios/48/linkedin-circled--v1.png"
                     alt="linkedin-logo"
                     class="icon"
                     />
                     </a>
                     <a target="blank" href="https://stackoverflow.com/users/18049556/vickiana">
-                    <img src="https://cdn.iconscout.com/icon/free/png-128/stack-overflow-3771544-3149964.png"
+                    <img src="https://cdn.iconscout.com/icon/free/png-48/stack-overflow-3771544-3149964.png"
                     alt="stackoverflow-logo"
                     class="icon"
                     />
